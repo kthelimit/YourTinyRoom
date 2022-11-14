2f49b5acf1132dc009f994e5cbc636399bf5c875
K <73793167+kthelimit@users.noreply.github.com>
2022-11-14 18:04:45 +0900

    UI일부 구현
    세팅창(볼륨) 완료
    퀘스트 창 구현중

---
+++ YourTinyRoom/Assets/04.Scripts/GameControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject PauseImg;
+    public RectTransform SettingMenu;
+
+    void Start()
+    {
+        PauseImg = GameObject.Find("Canvas-UI").transform.GetChild(6).gameObject;
+        SettingMenu = PauseImg.transform.GetChild(0).GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (Application.platform==RuntimePlatform.WindowsEditor) //윈도우 버전에서만 ESC에 반응
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if(PauseImg.gameObject.activeInHierarchy==false)
+        {
+            if (SettingMenu.gameObject.activeInHierarchy == false)
+            {
+                SettingMenu.gameObject.SetActive(true);
+            }
+            PauseImg.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            PauseImg.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+    }
+}
---
+++ YourTinyRoom/Assets/04.Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public float sfxVolume = 1.0f; // 볼륨
+    public bool isSfxMute = false; //뮤트여부
+    public static SoundManager soundManager;//싱글턴
+    public Slider audioslider;
+
+    void Awake()
+    {
+        if (soundManager == null)
+            soundManager = this;
+        else if (soundManager != this)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlaySfx(Vector3 pos, AudioClip sfx)
+    {
+        if (isSfxMute) return;
+        GameObject soundobj = new GameObject("Sfx");
+        soundobj.transform.position = pos;
+        AudioSource audioSource = soundobj.AddComponent<AudioSource>();
+        audioSource.clip = sfx;
+        audioSource.minDistance = 10f;
+        audioSource.maxDistance = 40f;
+        audioSource.volume = sfxVolume;
+        audioSource.Play();
+        Destroy(soundobj, sfx.length);
+    }
+
+    public void SetVolume(float sliderValue)
+    {
+        sfxVolume = sliderValue;
+    }
+
+    public void PlaySfx(Vector3 pos, AudioClip bgm, bool loop)
+    {
+        if (isSfxMute) return;
+        GameObject soundobj = new GameObject("Sfx");
+        soundobj.transform.position = pos;
+        AudioSource audioSource = soundobj.AddComponent<AudioSource>();
+        audioSource.clip = bgm;
+        audioSource.minDistance = 10f;
+        audioSource.maxDistance = 40f;
+        audioSource.volume = sfxVolume;
+        audioSource.loop = loop;
+        audioSource.Play();
+    }
+
+}
